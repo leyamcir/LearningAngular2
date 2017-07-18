f02e1730b65a9f2a2a424617d14fcbe54d2986b6
Alicia Daza <amdazacastillo@gmail.com>
2017-07-18 23:37:29 +0200

    L1S1 Making It Dynamic - > init

--- Level1/Section1/app/main.ts
+++ Level1/Section1/app/main.ts
@@ -4,16 +4,28 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @Component({
   selector: 'racing-app',
-  template: '<h1>{{heading}}</h1>'
+  template: `
+    <h1>{{ heading }}</h1>
+    <h2><!-- Name --></h2>
+    <p><!-- Date --></p>
+    <p><!-- About --></p>
+  `
 })
 class AppComponent {
   heading = "Ultra Racing Schedule"
+  race = {
+    "id": 1,
+    "name": "Daytona Thunderdome",
+    "date": new Date('2512-01-04T14:00:00'),
+    "about": "Race through the ruins of an ancient Florida battle arena.",
+    "entryFee": 3200
+  }
 }
 
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [ AppComponent ]
 })
 class AppModule {}
 
