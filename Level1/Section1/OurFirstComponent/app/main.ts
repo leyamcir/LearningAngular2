fe2f0debf86442d84a8e5c08ed914b99084d1316
Alicia Daza <amdazacastillo@gmail.com>
2017-07-18 23:27:49 +0200

    L1S1 Our first component -> Task 2/4 Inside the main.ts file, write out a @Component decorator, which has a selector for racing-app, and prints out the heading component property in an h1 tag.

--- Level1/Section1/OurFirstComponent/app/main.ts
+++ Level1/Section1/OurFirstComponent/app/main.ts
@@ -2,6 +2,10 @@ import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+@Component({
+  selector: 'racing-app',
+  template: '<h1>{{heading}}</h1>'
+})
 class AppComponent {
   heading = "Ultra Racing Schedule"
 }
